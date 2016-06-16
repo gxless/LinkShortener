69c26c70f4ef0caa1e0833859070b2dfd5bb1c1a
gxless <hitgx@yahoo.com>
2016-06-16 18:27:15 -0400

    added a cute status notification

--- views/app.js
+++ views/app.js
@@ -10,13 +10,21 @@
 
         $scope.getShortenedLink = function(originalLink) {
 
+            $scope.requested = false;
+            $scope.complete = false;
+
             if(!$scope.linkForm.$valid) {
+                $scope.requeted = false;
+                $scope.complete = false;
                 $scope.showError = true;
                 $scope.error = '您输入的URL格式不正确,请重新输入';
                 $scope.shortenedLink = '';
             } else {
+                $scope.requested = true;
+                $scope.complete = false;
                 $scope.showError = false;
                 $scope.error = '';
+
                 var reqLink = {
                     'originalLink': originalLink
                 };
@@ -24,6 +32,7 @@
                 LinkService.getShortenedLink(reqLink)
                     .then(function (response){
                         $scope.shortenedLink = response;
+                        $scope.complete = true;
                     });
             }
 
